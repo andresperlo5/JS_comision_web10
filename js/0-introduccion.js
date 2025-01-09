0a02e95186af2959e2cf02fbc5e0c1e5e0baa2f6
unknown <andresperlo5@gmail.com>
2025-01-09 11:26:00 -0300

    introduccion a JS

---
+++ js/0-introduccion.js
@@ -0,0 +1,81 @@
+/* Atributos - propiedades */
+/* Metodo - funcion - se deben ejecutar() */
+/* console.log("hola mundo"); */
+
+/* 
+Variable
+1- Inicializar - let / const / var
+2- Se le asigna un nombre - 
+3- Se le asigna un valor
+*/
+
+/* LET - Se lo usa cuando el valor de la variable puede llegar a mutar */
+/* let nombreDelUsuario = "Andres";
+console.log(nombreDelUsuario);
+nombreDelUsuario = "Juan";
+console.log(nombreDelUsuario); */
+
+/* CONST - No muta - No se puede modificar */
+/* const apellidoDelUsuario = "Perlo";
+console.log(apellidoDelUsuario); */
+
+/* Tipos de Datos */
+/* Strings - Cadenas de texto -'' "" -  `` -*/
+/* let nombre = "Andres";
+let apellido = "Perlo"; */
+/* let nombreCompleto = nombre + " " + apellido; */
+/* let nombreCompleto = `<h1>Hola <i><u>${nombre} ${apellido}</u></i> bienvenido a mi pagina </h1>`;
+console.log(nombreCompleto);
+document.write(nombreCompleto); */
+
+/* Numbers - Numeros */
+//const edad = 33;
+/* operadores aritmeticos 
++ - * / %
+
+operadores relacionales
+= Asignar
+== comparan solo el valor 
+===  comparan el valor y el tipo de dato
+
+!= distinto - comparan solo el valor 
+!== distinto  - comparan el valor y el tipo de dato
+
+> Mayor
+< Menor
+
+&& - AND - Y
+|| - OR - O
+! - invierte el valor de un booleano
+*/
+/* const numeroMinimo = Math.min(5, 3, 96, 25);
+console.log(numeroMinimo);
+const numeroMaximo = Math.max(5, 3, 96, 25);
+console.log(numeroMaximo);
+console.log(33 === "33"); */
+
+/* Booleanos - true o false */
+/* let terminosYcondiciones = true;
+console.log(terminosYcondiciones);
+console.log(!terminosYcondiciones); */
+
+/* Array - Arreglos - Organizar */
+//const ejemplo = [
+/* Elemento - indice - index - i*/
+//];
+/* const alumnos = ["Julieta", "Francisco", "Carolina"];
+console.log(alumnos);
+document.write(alumnos);
+ */
+/* Objetos */
+//const persona = {
+/* Propiedades  */
+/* Clave : Valor */
+/*   nombre: "Andres",
+  apellido: "Perlo",
+  dni: "12345678",
+  domicilio: "Las talitas",
+}; */
+
+//console.log(persona);
+//document.write(persona);
