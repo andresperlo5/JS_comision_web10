13d31fe9b5bac2a0f32bc96d10b31e8e7c288145
unknown <andresperlo5@gmail.com>
2025-01-14 11:28:42 -0300

    clase de string y numbers

---
+++ js/1-String.js
@@ -0,0 +1,67 @@
+/* String */
+//const nombreUsuario = "Andres Perlo".trim();
+//console.log(nombreUsuario.length);
+/* Andres123 - andres123 - ANDRES123 - AnDreS123 */
+//console.log(nombreUsuario.toLowerCase()); /* Minuscula */
+//console.log(nombreUsuario.toUpperCase()); /* Mayuscula */
+
+// Crear un comentario de una linea
+/* Crear un comentario en multilinea */
+//alert("Hola");
+/* const usuario = prompt("Escribe un usuario").trim();
+const contrasenia = prompt("Escribe tu contraseña").trim();
+console.log(usuario); */
+//const numero1 = prompt("Ingresa el primer numero").trim();
+//const numero2 = prompt("Ingresa el segundo numero").trim();
+//const suma = numero1 + numero2;
+//console.log(suma);
+// 12 - '12' + '12' - '1212'
+//parseInt - parseFloat - Number
+/* 
+parseInt - transforma un numero string a numero tipo number
+parseFloat - numeros entero y decimal
+Number - numeros enteros y decimales
+
+*/
+
+/* 
+const numero = "15.16";
+console.log(parseInt(numero));
+console.log(parseFloat(numero));
+console.log(Number(numero));
+ */
+
+/* const numero1 = 12;
+const numero2 = 6;
+
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+console.log(numero1 + numero2);
+ */
+
+/* const numero1 = "12";
+const numero2 = 6;
+
+document.write(`${numero1 - numero2} <br>`);
+document.write(`${numero1 * numero2} <br>`);
+document.write(`${numero1 / numero2} <br>`);
+document.write(`${numero1 + numero2} <br>`); */
+
+/*  length y substring o charAt  */
+//const nombreUsuario = "Andres Perlo".trim();
+//console.log(nombreUsuario.length);
+/* 
+1   2   3   4   5   6   7  - length 6 caracteres
+A - N - D - R - E - S -   -
+0   1   2   3   4   5   6- CharAt - Indice - Caracter
+*/
+//console.log(nombreUsuario.charAt(0));
+//const nombreCorto = nombreUsuario.substring(0, 3);
+//console.log(nombreCorto);
+//console.log(nombreUsuario);
+
+/* HOLA - H-O-L-A */
+/* const saludo = "Hola comision web 9";
+const separarSaludo = saludo.split("");
+const unirSaludo = separarSaludo.join("-"); */
---
+++ js/2-Numbers.js
@@ -0,0 +1,23 @@
+/* Number */
+/* Redondear un numero decimal */
+//const numero = 10.5;
+
+/* 
+ Floor - redondea al entero actual
+ Ceil - redondea al entero siguiente
+ Round - depende del decimal para redondear
+*/
+
+/* console.log(Math.floor(numero));
+console.log(Math.ceil(numero));
+console.log(Math.round(numero));
+ */
+/*  Crear un numero aleatorio */
+
+/* const numeroAleatorio = Math.random() * 10;
+const numeroEntero = Math.round(numeroAleatorio);
+console.log(numeroAleatorio);
+console.log(numeroEntero); */
+
+/* const numeroDecimal = (10.5959595959595595959).toFixed(2);
+console.log(Number(numeroDecimal)); */
